2dd992924636ba2231db52900ce7fc340c354b3c
Geoffrey Ostosh <ostoshg@gmail.com>
2016-09-22 20:17:25 -0700

    add deep clone to replace jQuery implementation

--- jStore.js
+++ jStore.js
@@ -1,6 +1,7 @@
 (function( window ) {
   var
     isSizeLimitException,
+    clone,
     createStoreObject,
     isActiveStore,
     getStoreData,
@@ -29,20 +30,58 @@
     }
     return false;
   }
+  
+  /**
+  * Deep clone given object
+  *
+  * @param {Object} content The object
+  * to be cloned
+  */
+  clone = function(content) {
+    var copy;
+
+    // Handle the 3 simple types, and null or undefined
+    if (null == content || 'object' !== typeof content) 
+      return content;
+
+    // clone Date
+    if (content instanceof Date) {
+        copy = new Date();
+        copy.setTime(content.getTime());
+        return copy;
+    }
+
+    // clone Array
+    if (content instanceof Array) {
+        copy = [];
+        for (var i = 0, len = content.length; i < len; i++) {
+            copy[i] = clone(content[i]);
+        }
+        return copy;
+    }
+
+    // clone Object
+    if (content instanceof Object) {
+        copy = {};
+        for (var attr in content) {
+            if (content.hasOwnProperty(attr)) 
+              copy[attr] = clone(content[attr]);
+        }
+        return copy;
+    }
+  };
 
   /**
   * Creates local store object
   * for given content
   *
-  * @param {} content The store object
+  * @param {Object} content The store object
   * data to be stored
   */
   createStoreObject = function(content){
     var payload;
-    if(content instanceof Array)
-      payload = $.extend(true, [], content);
-    else if(typeof content === 'object')
-      payload = $.extend(true, {}, content);
+    if('object' === typeof content)
+      payload = clone(content);
     else
       payload = content;
     return {
@@ -79,7 +118,7 @@
   * id and data.
   *
   * @param {String} id Store object id
-  * @param {} data Store object data
+  * @param {Object} data Store object data
   */
   setStoreData = function(id, data){
     var storeObject = createStoreObject(data);
@@ -97,7 +136,7 @@
   * id and data.
   *
   * @param {String} id Store object id
-  * @param {} data Store object data
+  * @param {Object} data Store object data
   */
   updateStoreData = function(id, data){
     deleteStoreData(id);
